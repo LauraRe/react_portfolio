9e2ed27cf470d37b25533db45e133bbe872b2974
Laura Reale <laaru1988@gmail.com>
2019-02-05 10:33:48 +0100

    add basic styling to header and footer tags

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,9 +2,13 @@ import React from "react"
 
 const Footer = () => {
     return (
-        <footer>
-            <h1>Made with React</h1>
-        </footer>
+        <div class="border-t border-smoke px-8 py-4">
+            <div className="flex justify-center text-grey">
+                <footer>
+                    <h1>Made with React</h1>
+                </footer>
+            </div>
+        </div >
     )
 }
 
