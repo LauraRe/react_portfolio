ebefc4933a6b523d1c47da07d2217e9a5b892cba
Laura Reale <laaru1988@gmail.com>
2019-02-05 10:43:48 +0100

    add apply directive to bundle custom Tailwind classes

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,11 +2,9 @@ import React from "react"
 
 const Footer = () => {
     return (
-        <div class="border-t border-smoke px-8 py-4">
-            <div className="flex justify-center text-grey">
-                <footer>
-                    <h1>Made with React</h1>
-                </footer>
+        <div class="footer-wrapper">
+            <div className="footer-content">
+                Made with React
             </div>
         </div >
     )
