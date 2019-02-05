827c83cd327a98ff21b435a0531266edecfbf13c
Laura Reale <laaru1988@gmail.com>
2019-02-05 11:30:13 +0100

    add React Router routing library, create About and Projects components, update React-DOM.render with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import Hello from "./Hello"
 import Header from "./Header"
 import Footer from "./Footer"
 import './css/tailwind.css'
+import { BrowserRouter } from 'react-router-dom'
+import Projects from "./Projects"
+import About from "./About"
 
 const App = () => {
     return (
@@ -19,4 +22,8 @@ const App = () => {
     )
 }
 
-ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render((
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+), document.getElementById("app"));
